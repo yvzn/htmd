84571c3d4a549722d86d4a593a516d7d206a8b9b
yvzn <mail@ludeo.net>
2023-12-13 18:24:40 +0100

    fix: replace nav anchors with explicit tabs

--- main.js
+++ main.js
@@ -7,14 +7,11 @@ import { CustomPreview } from './custom-preview';
 
 import './style.css';
 
-const navigationLinks = document.querySelectorAll('nav a');
+const navigationTabs = document.querySelectorAll('nav input[name="nav-tab"]');
 
-for (let el of navigationLinks) {
-  el.addEventListener('click', function (event) {
-    for (let link of navigationLinks) {
-      link.classList.remove('active');
-    }
-    event.target.classList.add('active');
+for (let el of navigationTabs) {
+  el.addEventListener('change', function (event) {
+    event.target.dispatchEvent(new CustomEvent('switch-tab', { bubbles: true, detail: { sectionId: event.target.value } }))
   });
 }
 
@@ -39,49 +36,49 @@ const converters = {
 }
 
 let lastModified = null
+let lastTab = null
 
 customElements.define('custom-editor-view', CustomEditorView)
 customElements.define('custom-preview', CustomPreview)
 
-document.getElementById('html-link').addEventListener('click', function () {
-  if (!editors.html) {
-    editors.html = document.createElement('custom-editor-view')
-    editors.html.setAttribute('language', 'html')
-    editors.html.addEventListener('change', function () { lastModified = 'html' })
-    document.getElementById('html').replaceChildren(editors.html);
+document.addEventListener('switch-tab', function (event) {
+  if (lastTab) {
+    lastTab.style.display = 'none'  
+  }
+  lastTab = document.getElementById(event.detail.sectionId)
+  lastTab.style.display = 'block'
+
+  if (event.detail.sectionId === 'html') {
+    showEditor(event.detail.sectionId)
+  }
+  if (event.detail.sectionId === 'markdown') {
+    showEditor(event.detail.sectionId)
+  }
+  if (event.detail.sectionId === 'preview') {
+    showPreview()
+  }
+})
+
+function showEditor(language) {
+  if (!editors[language]) {
+    editors[language] = document.createElement('custom-editor-view')
+    editors[language].setAttribute('language', language)
+    editors[language].addEventListener('change', function () { lastModified = language })
+    document.getElementById(language).replaceChildren(editors[language]);
   }
 
-  if (!lastModified || lastModified === 'html') {
+  if (!lastModified || lastModified === language) {
     return;
   }
 
-  const converter = converters[lastModified].to.html
+  const converter = converters[lastModified].to[language]
   const originalContent = editors[lastModified].content
   const convertedContent = converter(originalContent)
 
-  editors.html.content = convertedContent
-})
+  editors[language].content = convertedContent
+}
 
-document.getElementById('markdown-link').addEventListener('click', function () {
-  if (!editors.markdown) {
-    editors.markdown = document.createElement('custom-editor-view')
-    editors.markdown.setAttribute('language', 'markdown')
-    editors.markdown.addEventListener('change', function () { lastModified = 'markdown' })
-    document.getElementById('markdown').replaceChildren(editors.markdown);
-  }
-
-  if (!lastModified || lastModified === 'markdown') {
-    return;
-  }
-
-  const converter = converters[lastModified].to.markdown
-  const originalContent = editors[lastModified].content
-  const convertedContent = converter(originalContent)
-
-  editors.markdown.content = convertedContent
-})
-
-document.getElementById('preview-link').addEventListener('click', function () {
+function showPreview() {
   if (!editors.preview) {
     editors.preview = document.createElement('custom-preview')
     document.getElementById('preview').replaceChildren(editors.preview);
@@ -99,38 +96,38 @@ document.getElementById('preview-link').addEventListener('click', function () {
   }
 
   editors.preview.setAttribute('content', content)
+};
+
+document.getElementById('image-button').addEventListener('click', function () {
+  var image = document.getElementById('image-tag');
+  var errorMessage = document.getElementById('image-error');
+  var textarea = document.getElementById('image-document');
+
+  [image, errorMessage, textarea].map(el => el.style.display = 'none');
+
+  ClipboardUtils.readImage(function (data, error) {
+    if (error) {
+      console.error(error);
+      errorMessage.style.display = 'block';
+      errorMessage.innerHTML = error.message;
+      return;
+    }
+    if (data) {
+      image.src = data;
+      image.style.display = 'block';
+      textarea.value = `![Alt text](${data})`;
+      textarea.style.display = 'block';
+      errorMessage.innerHTML = '';
+      return;
+    }
+    errorMessage.style.display = 'block';
+    errorMessage.innerHTML =
+      'No image available in the clipboard - copy it manually...';
+  });
 });
 
-  document.getElementById('image-button').addEventListener('click', function () {
-    var image = document.getElementById('image-tag');
-    var errorMessage = document.getElementById('image-error');
-    var textarea = document.getElementById('image-document');
-
-    [image, errorMessage, textarea].map(el => el.style.display = 'none');
-
-    ClipboardUtils.readImage(function (data, error) {
-      if (error) {
-        console.error(error);
-        errorMessage.style.display = 'block';
-        errorMessage.innerHTML = error.message;
-        return;
-      }
-      if (data) {
-        image.src = data;
-        image.style.display = 'block';
-        textarea.value = `![Alt text](${data})`;
-        textarea.style.display = 'block';
-        errorMessage.innerHTML = '';
-        return;
-      }
-      errorMessage.style.display = 'block';
-      errorMessage.innerHTML =
-        'No image available in the clipboard - copy it manually...';
-    });
+document
+  .getElementById('image-document')
+  .addEventListener('click', function (event) {
+    event.target.select();
   });
-
-  document
-    .getElementById('image-document')
-    .addEventListener('click', function (event) {
-      event.target.select();
-    });
