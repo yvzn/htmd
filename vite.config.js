524ded24e3a77f332c1704cebba6a123f8f2bdc0
yvzn <mail@ludeo.net>
2023-12-21 15:18:40 +0100

    chore: code splitting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     const githubPagesBasePath = '/html-to-markdown/'
     return {
         base: mode === 'production' ? githubPagesBasePath : '/',
+        plugins: [splitVendorChunkPlugin()],
+        build: {
+           rollupOptions: {
+             output: {
+               manualChunks(id) {
+                 if (id.includes('asciidoctor')) {
+                   return '@asciidoctor';
+                 }
+                 if (id.includes('codemirror')) {
+                    return '@codemirror';
+                  }
+                },
+             },
+           },
+         },
     }
 })
